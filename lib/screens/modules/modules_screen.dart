effc2a98be7cd596a05aa6091b36a58f5e6cd836
rs-mintt <mintt@runsystem.net>
2022-03-10 00:13:53 +0700

    Modules screen

--- lib/screens/modules/modules_screen.dart
+++ lib/screens/modules/modules_screen.dart
@@ -28,33 +28,51 @@ class ModulesScreen extends StatelessWidget with Utils {
           Switch.adaptive(value: true, onChanged: (value) {}),
         ],
       ),
-      body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-          child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-              ),
-              itemCount: listModules.length,
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index) => _buildGridItem(
-                  context,
-                  listModules.keys.elementAt(index),
-                  listModules.values.elementAt(index)))),
+      body: Center(
+        child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: GridView.builder(
+                clipBehavior: Clip.none,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 15,
+                  mainAxisSpacing: 15,
+                ),
+                itemCount: listModules.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) =>
+                    _buildGridItem(context, listModules.keys.elementAt(index),
+                        listModules.values.elementAt(index)))),
+      ),
     );
   }
 
   Widget _buildGridItem(BuildContext context, String title, String routeName) {
     return InkWell(
       onTap: () => pushName(context, routeName),
-      child: Card(
-        elevation: 10.0,
-        shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-        child: Container(
-          alignment: Alignment.center,
-          child: Text(title),
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(
+          title,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.grey[300],
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[500]!,
+              offset: const Offset(4, 4),
+              blurRadius: 15,
+              spreadRadius: 1,
+            ),
+            const BoxShadow(
+              color: Colors.white,
+              offset: Offset(-4, -4),
+              blurRadius: 15,
+              spreadRadius: 1,
+            ),
+          ],
         ),
       ),
     );
